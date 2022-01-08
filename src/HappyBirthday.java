e17f50cacae7f55868c56b3e1fb3f467cd59bbcc
Julia <juliamariajoseph@gmail.com>
2022-01-08 16:45:41 +0530

    Initial push

--- src/HappyBirthday.java
+++ src/HappyBirthday.java
@@ -1,2 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+        import static java.lang.System.*;
+
 public class HappyBirthday {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        System.in));
+
+        String[] input = new String[2];
+        String a;
+        out.print("Please enter two integers: ");
+        input = in.readLine().split(" ");
+
+        a = String.valueOf(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c=2022-b;
+
+        out.println("Happy Birthday "+a+"! Your age is "+c);
+    }
 }
