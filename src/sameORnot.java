37ca2141a428c0b7f3ca504ecb9c133fb2874876
Julia <juliamariajoseph@gmail.com>
2022-01-11 13:01:13 +0530

    Initial push

--- src/sameORnot.java
+++ src/sameORnot.java
@@ -1,22 +1,22 @@
 
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
-public class sameORnot {
+//public class sameORnot {
 
-    public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
-        System.out.print("a=");
-        int a = num.nextInt();
-        System.out.print("b=");
-        int b = num.nextInt();
+    //public static void main(String[] args) {
+        //Scanner num = new Scanner(System.in);
+        //System.out.print("a=");
+        //int a = num.nextInt();
+        //System.out.print("b=");
+        //int b = num.nextInt();
 
-        if (a == b) {
-            System.out.println("a and b are same numbers");
-        } else {
-            System.out.println("a and b are not same numbers");
-        }
-    }
-}
+        //if (a == b) {
+          //  System.out.println("a and b are same numbers");
+        //} else {
+          //  System.out.println("a and b are not same numbers");
+        //}
+    //}
+//}
 
 
